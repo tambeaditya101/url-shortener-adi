9c2b7a48c1d7e5a02ac3637a4db3c9a728cc5a9f
adityatambe-personal <tambeaditya101@gmail.com>
2025-08-06 03:04:42 +0530

    fixed login prod issue

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -3,6 +3,6 @@
 export const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === 'production',
-  sameSite: 'lax',
+  sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
   maxAge: 1000 * 60 * 60 * 24, // 24 hours
 };
