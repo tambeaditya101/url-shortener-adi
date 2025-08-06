00cdf2d24f571e1dbfff45951a60dee203b701a4
adityatambe-personal <tambeaditya101@gmail.com>
2025-08-06 13:08:52 +0530

    ios issue testing on prod

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -3,6 +3,6 @@
 export const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === 'production',
-  sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+  sameSite: 'lax',
   maxAge: 1000 * 60 * 60 * 24, // 24 hours
 };
